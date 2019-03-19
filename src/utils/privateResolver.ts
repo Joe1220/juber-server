5a1a22ba3b01897b1a7498ce8395d3d02050f9e2
joe1220 <joe1220@daum.net>
2019-03-19 16:08:45 +0900

    Protecting Resolvers with Middlewares

---
+++ src/utils/privateResolver.ts
@@ -0,0 +1,9 @@
+const authResolver = (resolverFunction) => async(parent, args, context, info) => {
+  if(!context.req.user) {
+    throw new Error("No JWT. I refuse to proceed")
+  }
+  const resolved = await resolverFunction;
+  return resolved
+}
+
+export default authResolver;
