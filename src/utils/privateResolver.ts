9ccf0b777f3548dd283dd9729427aeff3381a050
joe1220 <joe1220@daum.net>
2019-03-19 16:12:53 +0900

    apply resolverFunction argument

--- src/utils/privateResolver.ts
+++ src/utils/privateResolver.ts
@@ -2,7 +2,7 @@ const authResolver = (resolverFunction) => async(parent, args, context, info) =>
   if(!context.req.user) {
     throw new Error("No JWT. I refuse to proceed")
   }
-  const resolved = await resolverFunction;
+  const resolved = await resolverFunction(parent, args, context, info);
   return resolved
 }
 
