891ae056b8a6190c144a9f5e6c9b78914fbec0d6
joe1220 <joe1220@daum.net>
2019-03-19 16:18:46 +0900

    resolver name bug

--- src/utils/privateResolver.ts
+++ src/utils/privateResolver.ts
@@ -1,9 +1,14 @@
-const authResolver = (resolverFunction) => async(parent, args, context, info) => {
-  if(!context.req.user) {
-    throw new Error("No JWT. I refuse to proceed")
+const privateResolver = resolverFunction => async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  if (!context.req.user) {
+    throw new Error("No JWT. I refuse to proceed");
   }
   const resolved = await resolverFunction(parent, args, context, info);
-  return resolved
-}
+  return resolved;
+};
 
-export default authResolver;
+export default privateResolver;
