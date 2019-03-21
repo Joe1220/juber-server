7bb1c7b86983bf15f0db2541934abba6331e2ea5
joe1220 <joe1220@daum.net>
2019-03-21 13:41:06 +0900

    ToggleDrivingMode Resolver

---
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -0,0 +1,43 @@
+
+import User from "../../../entities/User";
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import cleanNullArgs from "../../../utils/cleanNullArg";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateMyProfile: privateResolver(
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        const user: User = req.user;
+        const notNull: any = cleanNullArgs(args);
+        try {
+          if (args.password !== null) {
+            user.password = notNull.password;
+            user.save();
+            delete notNull.password;
+          }
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
