ee10f5fab38582ed24a43b67e3b37eb93268331c
joe1220 <joe1220@daum.net>
2019-03-25 17:03:29 +0900

    Testing GetChat Resolver

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -1,3 +1,4 @@
+
 import Chat from "../../../entities/Chat";
 import User from "../../../entities/User";
 import { GetChatQueryArgs, GetChatResponse } from "../../../types/graph";
@@ -10,9 +11,12 @@ const resolvers: Resolvers = {
       async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
         const user: User = req.user;
         try {
-          const chat = await Chat.findOne({
-            id: args.chatId
-          });
+          const chat = await Chat.findOne(
+            {
+              id: args.chatId
+            },
+            { relations: ["messages"] }
+          );
           if (chat) {
             if (chat.passengerId === user.id || chat.driverId === user.id) {
               return {
