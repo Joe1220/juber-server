873b81f1d3efae211755d59332c2f3a0903b3a5e
joe1220 <joe1220@daum.net>
2019-03-25 16:11:05 +0900

    GetChat Resolver

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,7 +7,6 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn
@@ -72,8 +71,11 @@ class User extends BaseEntity {
   @Column({ type: "text", nullable: true })
   fbId: string;
 
-  @ManyToOne(type => Chat, chat => chat.participants)
-  chat: Chat;
+  @OneToMany(type => Chat, chat => chat.passenger)
+  chatsAsPassenger: Chat[];
+
+  @OneToMany(type => Chat, chat => chat.driver)
+  chatsAsDriver: Chat[];
 
   @OneToMany(type => Message, message => message.user)
   messages: Message[];
@@ -86,15 +88,6 @@ class User extends BaseEntity {
 
   @OneToMany(type => Place, place => place.user)
   places: Place[];
-
-  @Column({type: "double precision", nullable:true})
-  orientation:number;
-
-  @Column({type: "double precision", nullable:true})
-  lat:number;
-
-  @Column({type: "double precision", nullable:true})
-  lng:number;
 
   @CreateDateColumn() createdAt: string;
 
